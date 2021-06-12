570120374a625dff13ca426a76e11e6ec6d52966
bvgi <gabrielastolarska@gmail.com>
2021-06-12 17:19:25 +0200

    Ostateczna wersja projektu

---
+++ tests/testtype.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+ 
+char* summation(int num, float real1, int bool1, char *str){
+char *summation;
+if(num == 1){
+summation = "Hello";
+} else {
+summation = "World";
+}
+return summation;
+}
+
+
+int main(void){
+float a;
+int b;
+int c;
+a = 22.5f;
+a = a + summation(10, 2.0f, 1, "Hello");
+b = 1;
+c = 0;
+return 0; 
+}
